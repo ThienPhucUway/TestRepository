5e12321d70fc45562fbe71753dd722c7350ab5f9
Thien Phuc <phuctran.uway@gmail.com>
2024-05-03 15:52:18 +0700

    update controller and model

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Stores;
+
+class StoreController extends Controller
+{
+    //
+    
+    public function index(){
+        
+    }
+}
